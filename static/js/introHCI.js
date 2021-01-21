4148ad67d2a97b47cbcaa418f878dcf61c11ad73
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-01-21 10:37:31 -0800

    add js from lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,21 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("LOL u clicked this")
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
+		var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
 }
